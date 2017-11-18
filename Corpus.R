f7450494e68dc444326fd27436516104d59306c8
fnassur <faiz.ali.nassur.93@gmail.com>
2017-11-17 21:21:46 -0500

    changes

--- Corpus.R
+++ Corpus.R
@@ -2,14 +2,20 @@
 
 #install.package("quanteda")
 
+#library calls
+library(quanteda)
+library(stringr)
+library(NLP)
+library(tm)
+
 #creating corpus split at every space
 textbag <- str_split(amazon.df$review,pattern = "\\s+") #can also use tokens(amazon.df$review)
 #from list to character
 textbag <- unlist(textbag)
 
 #get positive and negative words
-poswords <- scan('poswords.txt', what = 'character', comment.char = ';') #edit to pref
-negwords <- scan('negwords.txt', what = 'character', comment.char = ';') #edit to pref
+poswords <- scan("D:/Coursework syllabus/quarter 1/MIS 612/Class project/Github/MIS612/poswords.txt", what = 'character', comment.char = ';') #edit to pref
+negwords <- scan("D:/Coursework syllabus/quarter 1/MIS 612/Class project/Github/MIS612/negwords.txt", what = 'character', comment.char = ';') #edit to pref
 
 #get number of positive words for each review
 pscore <- unlist(lapply(textbag,function(x){
@@ -31,5 +37,5 @@ topfeatures(mydfm,20) #frequency of top 20 words
 score = sum(!is.na(match(textbag,poswords))) - sum(!is.na(match(textbag,negwords)))
 
 #create a wordcloud with min frequency
-wordcloud(textbag, min.freq = 150)
-wordcloud(textbag, min.freq = 150, random.order = FALSE)
+textplot_wordcloud(mydfm, min.freq = 150)
+textplot_wordcloud(mydfm, min.freq = 150, random.order = FALSE, colors = rainbow(3))
